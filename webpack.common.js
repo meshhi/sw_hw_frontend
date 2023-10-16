afbbb78ca2dfa82edfc824084dd825c6a6aae5be
meshhi <yumythug@yandex.ru>
2023-10-16 23:24:54 +0300

    sw

--- webpack.common.js
+++ webpack.common.js
@@ -48,6 +48,7 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
     new WorkboxPlugin.GenerateSW({
+      swDest: './service-worker.js',
       clientsClaim: true,
       skipWaiting: true,
       cleanupOutdatedCaches: true,
